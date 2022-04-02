acda9c8d6f9ec312b8cecfaefa1091ecf4488e00
Matheus Tostes <matheustostes@hotmail.com>
2022-04-02 16:32:22 -0300

    Core extension

---
+++ content.js
@@ -0,0 +1,19 @@
+// const btnToggleDark = document.getElementById("toggleDark")
+// const pStatus = document.getElementById("status")
+
+// function setToggleDark () {
+//     if (pStatus.innerText === 'desativado'){
+//         pStatus.innerText = 'Ativado'
+//     } else if (pStatus.innerText === 'Ativado'){
+//         pStatus.innerText = 'Desativado'
+//     }
+// }
+
+// btnToggleDark.addEventListener('click', setToggleDark)
+
+document.addEventListener('DOMContentLoaded', function(){
+    const divStranger = document.getElementById("root").firstChild
+    
+    divStranger.style.display = 'none'
+
+})
